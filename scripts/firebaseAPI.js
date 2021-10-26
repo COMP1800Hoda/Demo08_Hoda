60d3847efd2ea1350c9ca47337ab5bfd199b1336
Hoda Rashedi <hrashedi1@bcit.ca>
2021-10-26 12:18:03 -0700

    login done

--- scripts/firebaseAPI.js
+++ scripts/firebaseAPI.js
@@ -1,12 +1,12 @@
 // Your web app's Firebase configuration
-var  firebaseConfig = {
-    apiKey: ,
-    authDomain: ",
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId: 
-  };
+var firebaseConfig = {
+  apiKey: "AIzaSyBlSYvyjhGOTxqOS8QnG-cyYWeZbuhM2U0",
+  authDomain: "demo8-hoda.firebaseapp.com",
+  projectId: "demo8-hoda",
+  storageBucket: "demo8-hoda.appspot.com",
+  messagingSenderId: "314801213269",
+  appId: "1:314801213269:web:08429c561af3e3b10729dc"
+};
   
 
   const app = firebase.initializeApp(firebaseConfig);
